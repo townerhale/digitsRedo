80d4891bb4ad368d2cfc4aa4f2163db8ed9cd29b
townerhale <townerhale@gmail.com>
2017-10-23 19:14:05 -1000

    part 2 Complete

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -15,6 +15,20 @@ FlowRouter.route('/list', {
   },
 });
 
+FlowRouter.route('/add-contact', {
+  name: 'Add_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Contact_Page' });
+  },
+});
+
+FlowRouter.route('/edit-contact/:id', {
+  name: 'Edit_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Contact_Page' });
+  },
+});
+
 FlowRouter.route('/add', {
   name: 'Add_Stuff_Page',
   action() {
